453da1045a4201bbe8225681c14c7242aba19caa
Paul Welby <pwelby@gmail.com>
2022-01-20 13:46:45 -0800

    updated key for firebase and removed api-key. Remove boilerplate code

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
-
-let config = {
+let  config = {
   apiKey: "xxx",
-  authDomain: "bezkoder-firebase.firebaseapp.com",
-  databaseURL: "https://bezkoder-firebase.firebaseio.com",
-  projectId: "bezkoder-firebase",
-  storageBucket: "bezkoder-firebase.appspot.com",
-  messagingSenderId: "xxx",
-  appId: "xxx",
+  authDomain: "react-firebase-crud-eedc4.firebaseapp.com",
+  projectId: "react-firebase-crud-eedc4",
+  storageBucket: "react-firebase-crud-eedc4.appspot.com",
+  messagingSenderId: "699098597040",
+  appId: "1:699098597040:web:ba8794b731386ea1e2dd75",
+  measurementId: "xxx"
 };
 
 firebase.initializeApp(config);
